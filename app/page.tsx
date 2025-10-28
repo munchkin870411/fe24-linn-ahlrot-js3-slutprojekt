0401a31be97138cbc4424e2a1e84d6d44e03aecb
Linn Ahlrot Sahlén <munchkin870411@gmail.com>
2025-10-28 12:27:32 +0100

    Add LoginReminder component

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,17 @@ import CountriesList from "../components/CountriesList/CountriesList";
 import SearchBar from "../components/SearchBar/SearchBar";
 import RegionFilter from "../components/RegionFilter/RegionFilter";
 import styles from "./page.module.css";
+import LoginReminder from "@/components/LoginReminder/LoginReminder";
+import { auth } from "@/auth";
+
 
 const HomePage = () => {
+  const session = auth();
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <div className={styles.searchWrapper}>
+          {!session && <LoginReminder />}
           <SearchBar />
         </div>
         <RegionFilter />
---
+++ components/LoginReminder/LoginReminder.tsx
@@ -0,0 +1,15 @@
+export default function LoginReminder() {
+  return (
+    <div style={{
+      background: '#fff3cd',
+      color: '#856404',
+      border: '1px solid #ffeeba',
+      borderRadius: 4,
+      padding: '12px 16px',
+      margin: '16px 0',
+      fontWeight: 500
+    }}>
+      You must be logged in to view country details.
+    </div>
+  );
+}
